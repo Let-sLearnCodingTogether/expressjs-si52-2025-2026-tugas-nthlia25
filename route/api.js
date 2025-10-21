9f7173cf78ef9204e61bcebd50e4e27f0794c838
nthlia25 <nathaliamargaretha2506@gmail.com>
2025-10-21 23:04:17 +0700

    Update api.js

--- route/api.js
+++ route/api.js
@@ -1,5 +1,11 @@
 import express from "express";
+import * as controllerDestinasiWisata from "../controllers/controllerDestinasiWisata.js"
 
 const api = express.Router()
+
+api.post("/destinasi_wisata", controllerDestinasiWisata.createNewDestinasiWisata)
+api.get("/destinasi_wisata", controllerDestinasiWisata.listDestinasiWisata)
+api.put("/destinasi_wisata/:id", controllerDestinasiWisata.updateDestinasiWisata)
+api.delete("/destinasi_wisata/:id", controllerDestinasiWisata.deleteDestinasiWisata)
 
 export default api
