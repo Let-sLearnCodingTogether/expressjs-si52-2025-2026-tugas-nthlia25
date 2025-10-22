bbdc9baf3be4670d3f6bd87bc38cc18dadbc10de
nthlia25 <nathaliamargaretha2506@gmail.com>
2025-10-22 22:57:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { database } from "./config/database.js"
+import database from "../config/database.js"
 
 const app = express()
 app.use(express.json())
@@ -7,6 +7,6 @@ app.use(express.json())
 app.use('/api', api)
 
 app.listen(3000, () => {
-    database()
+    database(18)
     console.log(`Aplikasi berjalan di http://localhost:3000`)
 })
